1f53e1a551801d1d802ac7ac4927935fbba51b24
Radosław Czubak (249380) <radoslaw.marek.czubak@gmail.com>
2020-07-29 12:55:56 +0200

    add map request

--- app/src/main/java/com/rczubak/parkhereapp/main/MainActivity.kt
+++ app/src/main/java/com/rczubak/parkhereapp/main/MainActivity.kt
@@ -2,11 +2,22 @@ package com.rczubak.parkhereapp.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.SupportMapFragment
 import com.rczubak.parkhereapp.R
 
 class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getMap()
     }
+
+    private fun getMap(){
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync{
+        }
+    }
+
+
 }
