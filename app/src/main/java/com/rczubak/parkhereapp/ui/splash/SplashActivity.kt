257597512eb91bd8a68546fc48cfabe2ca6de38c
Radosław Czubak <radoslaw.marek.czubak@gmail.com>
2020-08-21 15:38:31 +0200

    add splashscreen and new colours

---
+++ app/src/main/java/com/rczubak/parkhereapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.rczubak.parkhereapp.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rczubak.parkhereapp.R
+import com.rczubak.parkhereapp.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val mainActivityIntent = Intent(this, MainActivity::class.java)
+
+        startActivity(mainActivityIntent)
+    }
+}
